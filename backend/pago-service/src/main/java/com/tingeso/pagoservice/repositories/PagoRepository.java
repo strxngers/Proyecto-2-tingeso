90b1f2ab9bdf7b5ef6bdb3228a2fa150305021cf
Elena Guzmán <91577901+strxngers@users.noreply.github.com>
2023-06-15 22:42:53 -0400

    Se elimina la carpeta backend por generar errores y archivos incorrectos

--- backend/pago-service/src/main/java/com/tingeso/pagoservice/repositories/PagoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tingeso.pagoservice.repositories;
-
-import com.tingeso.pagoservice.entities.PagoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PagoRepository extends JpaRepository<PagoEntity,Integer> {
-}
