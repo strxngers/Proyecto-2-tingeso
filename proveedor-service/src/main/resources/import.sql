aaa32ff288710c6fe9772720d85d48a3c486c753
Elena Guzmán <91577901+strxngers@users.noreply.github.com>
2023-06-26 10:59:12 -0400

    Prometheus y grafana que no funcionan

--- proveedor-service/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO proveedor(id_proveedor,categoria,nombre,retencion) VALUES ("31003", "LA VACA LOLA", "A","SI");
-INSERT INTO proveedor(id_proveedor,categoria,nombre,retencion) VALUES ("12345", "LECHERIA", "B", "NO");
-INSERT INTO proveedor(id_proveedor,categoria,nombre,retencion) VALUES ("10035", "RUBIA GALLEGA", "C","SI");
-INSERT INTO proveedor(id_proveedor,categoria,nombre,retencion) VALUES ("12548", "LA VACA LECHERA", "D", "NO");
